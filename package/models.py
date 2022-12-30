a3abec31ee1535c0dd986a652340df56f0c9dd94
Rukaiya Amin <rukaiyaamin36@gmail.com>
2022-12-30 17:45:13 +0600

    database setup

---
+++ package/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+class Packages(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    details = models.TextField(blank=True, null=True)
+    service_ids = models.CharField(max_length=250, blank=True, null=True)
+    status = models.IntegerField()
+    created_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'packages'
+
+
+class Services(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    details = models.TextField(blank=True, null=True)
+    view_order = models.IntegerField()
+    status = models.IntegerField()
+    created_at = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'services'
